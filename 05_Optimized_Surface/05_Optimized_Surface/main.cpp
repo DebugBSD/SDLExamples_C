e906c0746ec6644cc28d9829eb27e09b7d8422fe
Guillermo Bernaldo de Quiros Maraver <debugbsd@gmail.com>
2020-11-01 10:14:11 +0100

    - Lesson 5 - Optimized Surface Loading and Soft Stretching (https://lazyfoo.net/tutorials/SDL/05_optimized_surface_loading_and_soft_stretching/index.php)

---
+++ 05_Optimized_Surface/05_Optimized_Surface/main.cpp
@@ -0,0 +1,165 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
+#include <SDL.h>
+#include <stdio.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+// Some global variables
+// The Window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+// The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+// Current displayed image
+SDL_Surface* gStretchedSurface = NULL;
+
+// Starts up SDL and creates the window
+bool Init();
+
+// Loads media
+bool LoadMedia();
+
+// Free media files and shut down SDL
+void Close();
+
+// Loads individual image
+SDL_Surface* loadSurface(const char* pPath);
+
+int main(int argc, char* args[])
+{
+	if (!Init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		// Load media
+		if (LoadMedia())
+		{
+			// Main loop flag
+			bool quit = false;
+
+			// Event Handler 
+			SDL_Event e;
+
+			// Handle events on queue
+			while (!quit)
+			{
+				while (SDL_PollEvent(&e) != 0)
+				{
+					// User requests to quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+				}
+
+				// Apply the image stretched
+				SDL_Rect stretchRect;
+				stretchRect.x = 0;
+				stretchRect.y = 0;
+				stretchRect.w = SCREEN_WIDTH;
+				stretchRect.h = SCREEN_HEIGHT;
+				SDL_BlitScaled(gStretchedSurface, NULL, gScreenSurface, &stretchRect);
+
+				// Update the surface 
+				SDL_UpdateWindowSurface(gWindow);
+			}
+		}
+	}
+
+	// Free resources and close SDL
+	Close();
+
+	return 0;
+}
+
+bool Init()
+{
+	// Initialization flag
+
+	bool success = true;
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+
+	return success;
+}
+
+bool LoadMedia(void)
+{
+	// Loading success flag
+	bool success = true;
+
+	gStretchedSurface = loadSurface("stretch.bmp");
+	if (!gStretchedSurface)
+	{
+		success = false;
+	}
+
+	
+	return success;
+}
+
+void Close()
+{
+	// Deallocate surface
+	SDL_FreeSurface(gStretchedSurface);
+	
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+SDL_Surface* loadSurface(const char* pPath)
+{
+	// The final optimized image
+	SDL_Surface* optimizedSurface = NULL;
+
+	// Load image at specified path
+	SDL_Surface* loadedSurface = SDL_LoadBMP(pPath);
+	if (!loadedSurface)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", pPath, SDL_GetError());
+	}
+	else
+	{
+		// Convert surface to screen format
+		optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
+		if (!optimizedSurface)
+		{
+			printf("Unable to optimize image %s!: SDL Error: %s\n", pPath, SDL_GetError());
+		}
+
+		// Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
+	}
+	return optimizedSurface;
+}
