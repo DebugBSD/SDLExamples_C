1840e34b85a302b4257c19e1e83e4f9ad9aca3ea
Guillermo Bernaldo de Quiros Maraver <debugbsd@gmail.com>
2020-11-02 08:39:13 +0100

    - Lesson 13 - Alpha Blending (https://lazyfoo.net/tutorials/SDL/13_alpha_blending/index.php)

---
+++ 13_Alpha_Blending/13_Alpha_Blending/main.cpp
@@ -0,0 +1,351 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <stdio.h>
+
+// Definitions
+// ----------------------------------------------------------------------------
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+// Texture wrapper class
+class LTexture
+{
+public:
+	// Initializes variables
+	LTexture();
+
+	// Deallocates memory
+	~LTexture();
+
+	// Loads image at specified path
+	bool loadFromFile(const char* pPath);
+
+	// Deallocate texture
+	void free();
+
+	// Set color modulation
+	void setColor(Uint8 red, Uint8 green, Uint8 blue);
+
+	// Set blending mode
+	void setBlendMode(SDL_BlendMode blending);
+
+	// Set alpha modulation
+	void setAlpha(Uint8 alpha);
+
+	// Render texture at given point
+	void render(int x = 0, int y = 0, SDL_Rect* clip = NULL);
+
+	// Gets image dimensions
+	int getWidth();
+	int getHeight();
+
+private:
+	// The actual hardware texture
+	SDL_Texture* m_pTexture;
+
+	// Image dimensions
+	int m_Width;
+	int m_Height;
+
+};
+
+// Some global variables
+// ----------------------------------------------------------------------------
+
+// The Window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+// The Window Renderer
+SDL_Renderer* gRenderer = NULL;
+
+// Scene textures
+LTexture gModulatedTexture;
+LTexture gBackgroundTexture;
+
+// Starts up SDL and creates the window
+bool Init();
+
+// Loads media
+bool LoadMedia();
+
+// Free media files and shut down SDL
+void Close();
+
+
+int main(int argc, char* args[])
+{
+	if (!Init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		// Load media
+		if (LoadMedia())
+		{
+			// Main loop flag
+			bool quit = false;
+
+			// Event Handler 
+			SDL_Event e;
+
+			// Modulation components
+			Uint8 a = 255;
+
+			// Handle events on queue
+			while (!quit)
+			{
+				while (SDL_PollEvent(&e) != 0)
+				{
+					// User requests to quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+					// On key press change rgb values
+					else if (e.type == SDL_KEYDOWN)
+					{
+						// Increase alpha on w
+						if (e.key.keysym.sym == SDLK_w)
+						{
+							// cap if over 255
+							if (a + 32 > 255)
+								a = 255;
+							else
+								a += 32;
+						}
+						else if(e.key.keysym.sym == SDLK_s)
+						{
+							// Cap if below 0
+							if (a - 32 < 0)
+								a = 0;
+							else
+								a -= 32;
+						}
+					}
+				}
+
+				// Clear Screen
+				SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
+				SDL_RenderClear(gRenderer);
+
+				// Render background
+				gBackgroundTexture.render();
+
+				// Render front blended
+				gModulatedTexture.setAlpha(a);
+				gModulatedTexture.render();
+
+				// Update screen
+				SDL_RenderPresent(gRenderer);
+			}
+		}
+	}
+
+	// Free resources and close SDL
+	Close();
+
+	return 0;
+}
+
+bool Init()
+{
+	// Initialization flag
+
+	bool success = true;
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (!gRenderer)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+				// Initialize renderer color
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+				// Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags))
+				{
+					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+bool LoadMedia(void)
+{
+	// Loading success flag
+	bool success = true;
+
+	// Load Foo texture
+	if (!gModulatedTexture.loadFromFile("fadeout.png"))
+	{
+		printf("Failed to load fadeout texture image!\n");
+		success = false;
+	}
+	else
+	{
+		// Set standard alpha blending
+		gModulatedTexture.setBlendMode(SDL_BLENDMODE_BLEND);
+	}
+
+	// Load background texture
+	if(!gBackgroundTexture.loadFromFile("fadein.png"))
+	{
+		printf("Failed to load fadein texture image!\n");
+		success = false;
+	}
+
+	// Nothing to load
+	return success;
+}
+
+void Close()
+{
+	// Deallocate surface
+	gModulatedTexture.free();
+
+	//Destroy window
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
+
+LTexture::LTexture()
+{
+	// Initialize
+	m_pTexture = NULL;
+	m_Width = m_Height = 0;
+}
+
+LTexture::~LTexture()
+{
+	// Deallocate 
+	free();
+}
+
+bool LTexture::loadFromFile(const char* pPath)
+{
+	// The final texture
+	SDL_Texture* newTexture = NULL;
+
+	// Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load(pPath);
+	if (!loadedSurface)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", pPath, SDL_GetError());
+	}
+	else
+	{
+		// Color key image
+		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+
+		// Create texture from surface pixels
+		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+		if (!newTexture)
+		{
+			printf("Unable to create texture from %s!: SDL Error: %s\n", pPath, SDL_GetError());
+		}
+		else
+		{
+			m_Width = loadedSurface->w;
+			m_Height = loadedSurface->h;
+		}
+
+		// Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
+	}
+	m_pTexture = newTexture;
+	return m_pTexture != NULL;
+}
+
+void LTexture::free()
+{
+	// Free texture if it exists
+	if (m_pTexture != NULL)
+	{
+		SDL_DestroyTexture(m_pTexture);
+		m_pTexture = NULL;
+		m_Width = m_Height = 0;
+	}
+}
+
+void LTexture::setColor(Uint8 red, Uint8 green, Uint8 blue)
+{
+	// Modulate texture
+	SDL_SetTextureColorMod(m_pTexture, red, green, blue);
+}
+
+void LTexture::setBlendMode(SDL_BlendMode blending)
+{
+	// Set blending function
+	SDL_SetTextureBlendMode(m_pTexture, blending);
+}
+
+void LTexture::setAlpha(Uint8 alpha)
+{
+	// Set texture alpha
+	SDL_SetTextureAlphaMod(m_pTexture, alpha);
+}
+
+void LTexture::render(int x, int y, SDL_Rect* pClip)
+{
+	// Get rendering space and render to screen;
+	SDL_Rect renderQuad{ x, y, m_Width, m_Height };
+
+	// Set clip rendering dimensions
+	if (pClip != NULL)
+	{
+		renderQuad.w = pClip->w;
+		renderQuad.h = pClip->h;
+	}
+
+	// Render to screen
+	SDL_RenderCopy(gRenderer, m_pTexture, pClip, &renderQuad);
+}
+
+int LTexture::getWidth()
+{
+	return m_Width;
+}
+
+int LTexture::getHeight()
+{
+	return m_Height;
+}
