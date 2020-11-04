652bbc2adb6562c3a8d4ad3229d4009c92e21b47
Guillermo Bernaldo de Quiros Maraver <debugbsd@gmail.com>
2020-11-04 10:12:13 +0100

    - Lesson 28 - Per-Pixel Collision Detection (https://lazyfoo.net/tutorials/SDL/28_per-pixel_collision_detection/index.php)

---
+++ 28_Per-Pixel_Detection/28_Per-Pixel_Detection/main.cpp
@@ -0,0 +1,835 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <vector>
+
+// Definitions
+// ----------------------------------------------------------------------------
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int WALKING_ANIMATION_FRAMES = 4;
+const int SCREEN_FPS = 60;
+const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
+
+// Button constants
+const int BUTTON_WIDTH = 300;
+const int BUTTON_HEIGHT = 200;
+const int TOTAL_BUTTONS = 4;
+
+enum LButtonSprite
+{
+	BUTTON_SPRITE_MOUSE_OUT = 0,
+	BUTTON_SPRITE_MOUSE_OVER_MOTION,
+	BUTTON_SPRITE_MOUSE_DOWN,
+	BUTTON_SPRITE_MOUSE_UP,
+	BUTTON_SPRITE_TOTAL
+};
+
+// The dot that will move around on the screen
+class Dot
+{
+public:
+	// The dimensions of the dot
+	static const int DOT_WIDTH = 20;
+	static const int DOT_HEIGHT = 20;
+
+	// Maximum axis velocity of the dot
+	static const int DOT_VEL = 10;
+
+	// Constructor
+	Dot(int x = 0, int y = 0);
+
+	// Takes key presses and adjusts the dot's velocity
+	void handleEvent(SDL_Event& e);
+
+	// Moves the dot and checks collision
+	void move(std::vector<SDL_Rect>& vecColliders);
+
+	// Shows the dot on the screen
+	void render();
+
+	// Gets the collision boxes
+	std::vector<SDL_Rect>& getColliders();
+
+private:
+
+	// Initialize the offsets
+	int	m_PosX;
+	int m_PosY;
+
+	// Velocity of the dot
+	int m_VelX;
+	int m_VelY;
+
+	// Dot's collision box
+	std::vector<SDL_Rect> m_Colliders;
+
+	// Moves the collision boxes relative to the dot's offset
+	void shiftColliders();
+};
+
+// Texture wrapper class
+class LTexture
+{
+public:
+	// Initializes variables
+	LTexture();
+
+	// Deallocates memory
+	~LTexture();
+
+	// Loads image at specified path
+	bool loadFromFile(const char* pPath);
+
+	// Create image from font string
+	bool loadFromRenderedText(const char* pTextureText, SDL_Color textColor);
+
+	// Deallocate texture
+	void free();
+
+	// Set color modulation
+	void setColor(Uint8 red, Uint8 green, Uint8 blue);
+
+	// Set blending mode
+	void setBlendMode(SDL_BlendMode blending);
+
+	// Set alpha modulation
+	void setAlpha(Uint8 alpha);
+
+	// Render texture at given point
+	void render(int x = 0, int y = 0, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* pCenter = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+	// Gets image dimensions
+	int getWidth();
+	int getHeight();
+
+private:
+	// The actual hardware texture
+	SDL_Texture* m_pTexture;
+
+	// Image dimensions
+	int m_Width;
+	int m_Height;
+
+};
+
+// The mouse button
+class LButton
+{
+public:
+	// Initializes internal variables
+	LButton();
+
+	// Sets top left position
+	void setPosition(int x = 0, int y = 0);
+
+	// Handles mouse event
+	void handleEvent(SDL_Event* e);
+
+	// Show button sprites
+	void render();
+
+private:
+	// Top left position
+	SDL_Point m_Position;
+
+	// Currently used global sprite
+	LButtonSprite m_CurrentSprite;
+};
+
+class LTimer
+{
+public:
+	// Initializes variables
+	LTimer();
+
+	// The various check actions
+	void start();
+	void stop();
+	void pause();
+	void resume();
+
+	// Gets the timer's time
+	Uint32 getTicks();
+
+	// Checks the status of the timer
+	bool isStarted() { return m_Started; }
+	bool isPaused() { return m_Paused && m_Started; }
+
+private:
+
+	// The clock time when the timer started
+	Uint32 m_StartTicks;
+
+	// The ticks stored when the timer was paused
+	Uint32 m_PausedTicks;
+
+	// The timer status
+	bool m_Paused;
+	bool m_Started;
+};
+
+// Some global variables
+// ----------------------------------------------------------------------------
+
+// The Window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+// The Window Renderer
+SDL_Renderer* gRenderer = NULL;
+
+// Globally used font
+TTF_Font* gFont = NULL;
+
+// Dot texture
+LTexture gDotTexture;
+
+// Starts up SDL and creates the window
+bool Init();
+
+// Loads media
+bool LoadMedia();
+
+// Free media files and shut down SDL
+void Close();
+
+// Box collision detector
+bool CheckCollision(std::vector<SDL_Rect>& a, std::vector<SDL_Rect>& b);
+
+int main(int argc, char* args[])
+{
+	if (!Init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		// Load media
+		if (LoadMedia())
+		{
+			// Main loop flag
+			bool quit = false;
+
+			// Event Handler 
+			SDL_Event e;
+
+			// The dot that will be moving around on the screen
+			Dot dot;
+
+			// The dot that will be collided against
+			Dot otherDot{ SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4 };
+
+			// Handle events on queue
+			while (!quit)
+			{
+				// Process input
+				while (SDL_PollEvent(&e) != 0)
+				{
+					// User requests to quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+
+					// Handle input fot the dot
+					dot.handleEvent(e);
+				}
+
+				// Update game
+				dot.move(otherDot.getColliders());
+
+				// Render game
+				// Clear Screen
+				SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
+				SDL_RenderClear(gRenderer);
+
+				// Render dots
+				dot.render();
+				otherDot.render();
+
+				// Draw HUD
+
+				// Update screen
+				SDL_RenderPresent(gRenderer);
+
+				// Other stuff
+
+			}
+		}
+	}
+
+	// Free resources and close SDL
+	Close();
+
+	return 0;
+}
+
+bool Init()
+{
+	// Initialization flag
+
+	bool success = true;
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+			if (!gRenderer)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+				// Initialize renderer color
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+				// Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags))
+				{
+					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+					success = false;
+				}
+
+				// Initialize SDL_ttf
+				if (TTF_Init() == -1)
+				{
+					printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+					success = false;
+				}
+
+				// Initialize SDL_Mixer
+				if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+				{
+					printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+bool LoadMedia(void)
+{
+	// Loading success flag
+	bool success = true;
+
+	if (!gDotTexture.loadFromFile("dot.bmp"))
+	{
+		success = false;
+	}
+
+	// Nothing to load
+	return success;
+}
+
+void Close()
+{
+	// Deallocate surface
+	gDotTexture.free();
+
+	//Destroy window
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	TTF_Quit();
+	IMG_Quit();
+	SDL_Quit();
+}
+
+bool CheckCollision(std::vector<SDL_Rect>& a, std::vector<SDL_Rect>& b)
+{
+	// The sides of the rectangles
+	int leftA, leftB;
+	int rightA, rightB;
+	int topA, topB;
+	int bottomA, bottomB;
+
+	// Go through the A boxes
+	for (int Abox = 0; Abox < a.size(); Abox++)
+	{
+		// Calculate the sides of rect A
+		leftA = a[Abox].x;
+		rightA = a[Abox].x + a[Abox].w;
+		topA = a[Abox].y;
+		bottomA = a[Abox].y + a[Abox].h;
+
+		// Go through the B boxes
+		for (int Bbox = 0; Bbox < b.size(); Bbox++)
+		{
+			// Calculate the sides of rect A
+			leftB = b[Bbox].x;
+			rightB = b[Bbox].x + b[Bbox].w;
+			topB = b[Bbox].y;
+			bottomB = b[Bbox].y + b[Bbox].h;
+
+			// If no sides from A are outside of B
+			if (((bottomA <= topB) || (topA >= bottomB) || (rightA <= leftB) || (leftA >= rightB)) == false)
+			{
+				// A collision is detected
+				return true;
+			}
+		}
+	}
+
+	// If neither set of collision boxes touched
+	return false;
+}
+
+
+LTexture::LTexture()
+{
+	// Initialize
+	m_pTexture = NULL;
+	m_Width = m_Height = 0;
+}
+
+LTexture::~LTexture()
+{
+	// Deallocate 
+	free();
+}
+
+bool LTexture::loadFromFile(const char* pPath)
+{
+	// The final texture
+	SDL_Texture* newTexture = NULL;
+
+	// Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load(pPath);
+	if (!loadedSurface)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", pPath, SDL_GetError());
+	}
+	else
+	{
+		// Color key image
+		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+
+		// Create texture from surface pixels
+		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+		if (!newTexture)
+		{
+			printf("Unable to create texture from %s!: SDL Error: %s\n", pPath, SDL_GetError());
+		}
+		else
+		{
+			m_Width = loadedSurface->w;
+			m_Height = loadedSurface->h;
+		}
+
+		// Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
+	}
+	m_pTexture = newTexture;
+	return m_pTexture != NULL;
+}
+
+bool LTexture::loadFromRenderedText(const char* pTextureText, SDL_Color textColor)
+{
+
+	// Get rid of preexisting texture
+	free();
+
+	// Render text surface
+	SDL_Surface* pTextSurface = TTF_RenderText_Solid(gFont, pTextureText, textColor);
+	if (!pTextSurface)
+	{
+		printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+	}
+	else
+	{
+		// Create texture from surface pixels
+		m_pTexture = SDL_CreateTextureFromSurface(gRenderer, pTextSurface);
+		if (!m_pTexture)
+		{
+			printf("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			// Get image dimensions
+			m_Width = pTextSurface->w;
+			m_Height = pTextSurface->h;
+		}
+
+		// Get rid of old surface
+		SDL_FreeSurface(pTextSurface);
+	}
+
+	return m_pTexture != NULL;
+
+	return true;
+}
+
+void LTexture::free()
+{
+	// Free texture if it exists
+	if (m_pTexture != NULL)
+	{
+		SDL_DestroyTexture(m_pTexture);
+		m_pTexture = NULL;
+		m_Width = m_Height = 0;
+	}
+}
+
+void LTexture::setColor(Uint8 red, Uint8 green, Uint8 blue)
+{
+	// Modulate texture
+	SDL_SetTextureColorMod(m_pTexture, red, green, blue);
+}
+
+void LTexture::setBlendMode(SDL_BlendMode blending)
+{
+	// Set blending function
+	SDL_SetTextureBlendMode(m_pTexture, blending);
+}
+
+void LTexture::setAlpha(Uint8 alpha)
+{
+	// Set texture alpha
+	SDL_SetTextureAlphaMod(m_pTexture, alpha);
+}
+
+void LTexture::render(int x, int y, SDL_Rect* pClip, double angle, SDL_Point* pCenter, SDL_RendererFlip flip)
+{
+	// Get rendering space and render to screen;
+	SDL_Rect renderQuad{ x, y, m_Width, m_Height };
+
+	// Set clip rendering dimensions
+	if (pClip != NULL)
+	{
+		renderQuad.w = pClip->w;
+		renderQuad.h = pClip->h;
+	}
+
+	// Render to screen
+	SDL_RenderCopyEx(gRenderer, m_pTexture, pClip, &renderQuad, angle, pCenter, flip);
+}
+
+int LTexture::getWidth()
+{
+	return m_Width;
+}
+
+int LTexture::getHeight()
+{
+	return m_Height;
+}
+
+LButton::LButton()
+{
+	m_Position.x = m_Position.y = 0;
+
+	m_CurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
+}
+
+void LButton::setPosition(int x, int y)
+{
+	m_Position.x = x;
+	m_Position.y = y;
+}
+
+void LButton::handleEvent(SDL_Event* e)
+{
+	// if mouse event happened
+	if (e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP)
+	{
+		// Check if mouse is in button
+		bool inside = true;
+
+		// Get mouse position
+		int x, y;
+		SDL_GetMouseState(&x, &y);
+
+		// Mouse is left of the button
+		if (x < m_Position.x)
+		{
+			inside = false;
+		}
+		// Mouse is right of the button
+		else if (x > m_Position.x + BUTTON_WIDTH)
+		{
+			inside = false;
+		}
+		// Mouse is above the button
+		else if (y < m_Position.y)
+		{
+			inside = false;
+		}
+		// Mouse is below the button
+		else if (y > m_Position.y + BUTTON_HEIGHT)
+		{
+			inside = false;
+		}
+		// Mouse is outside button
+		if (!inside)
+		{
+			m_CurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
+		}
+		// Mouse is inside button
+		else
+		{
+			// Set mouse over sprite
+			switch (e->type)
+			{
+			case SDL_MOUSEMOTION:
+				m_CurrentSprite = BUTTON_SPRITE_MOUSE_OVER_MOTION;
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+				m_CurrentSprite = BUTTON_SPRITE_MOUSE_DOWN;
+				break;
+			case SDL_MOUSEBUTTONUP:
+				m_CurrentSprite = BUTTON_SPRITE_MOUSE_UP;
+				break;
+			}
+		}
+	}
+}
+
+void LButton::render()
+{
+	// Show current button sprite
+	//gButtonSpriteSheetTexture.render(m_Position.x, m_Position.y, &gSpriteClips[m_CurrentSprite]);
+}
+
+LTimer::LTimer()
+{
+	// Initializes the variables;
+
+	m_StartTicks = m_PausedTicks = 0;
+	m_Paused = m_Started = false;
+}
+
+void LTimer::start()
+{
+	//Initialize the variables
+	m_StartTicks = SDL_GetTicks();
+	m_PausedTicks = 0;
+
+	m_Paused = false;
+	m_Started = true;
+}
+
+void LTimer::stop()
+{
+	//Initialize the variables
+	m_StartTicks = 0;
+	m_PausedTicks = 0;
+
+	m_Paused = false;
+	m_Started = false;
+}
+
+void LTimer::pause()
+{
+	// if the timer is running and isn't already paused
+	if (m_Started && !m_Paused)
+	{
+		// Pause the timer
+		m_Paused = true;
+
+		// Calculate the paused ticks
+		m_PausedTicks = SDL_GetTicks() - m_StartTicks;
+		m_StartTicks = 0;
+	}
+
+}
+
+void LTimer::resume()
+{
+	// If the timer is running and paused
+	if (m_Started && m_Paused)
+	{
+		// Unpause the timer
+		m_Paused = false;
+
+		// Reset the starting ticks
+		m_StartTicks = SDL_GetTicks() - m_PausedTicks;
+
+		// Reset paused ticks
+		m_PausedTicks = 0;
+	}
+}
+
+Uint32 LTimer::getTicks()
+{
+	// The actual timer time
+	Uint32 time = 0;
+
+	// If the timer is running
+	if (m_Started)
+	{
+		// If the timer is paused
+		if (m_Paused)
+		{
+			// Return the number of ticks when the timer was paused
+			time = m_PausedTicks;
+		}
+		else
+		{
+			// Return the current time minus the start time
+			time = SDL_GetTicks() - m_StartTicks;
+		}
+	}
+
+	return time;
+}
+
+Dot::Dot(int x, int y)
+{
+	m_PosX = x;  m_PosY = y;
+	m_VelX = m_VelY = 0;
+
+	// Set size of vector
+	m_Colliders.resize(11);
+
+	// Set collision box dimension
+	m_Colliders[0].w = 6;
+	m_Colliders[0].h = 1;
+
+	m_Colliders[1].w = 10;
+	m_Colliders[1].h = 1;
+
+	m_Colliders[2].w = 14;
+	m_Colliders[2].h = 1;
+
+	m_Colliders[3].w = 16;
+	m_Colliders[3].h = 2;
+
+	m_Colliders[4].w = 18;
+	m_Colliders[4].h = 2;
+
+	m_Colliders[5].w = 20;
+	m_Colliders[5].h = 6;
+
+	m_Colliders[6].w = 18;
+	m_Colliders[6].h = 2;
+
+	m_Colliders[7].w = 16;
+	m_Colliders[7].h = 2;
+
+	m_Colliders[8].w = 14;
+	m_Colliders[8].h = 1;
+
+	m_Colliders[9].w = 10;
+	m_Colliders[9].h = 1;
+
+	m_Colliders[10].w = 6;
+	m_Colliders[10].h = 1;
+
+	// Initialize colliders relative to position
+	shiftColliders();
+}
+
+void Dot::handleEvent(SDL_Event& e)
+{
+	// If a key was pressed
+	if (e.type == SDL_KEYDOWN && e.key.repeat == 0)
+	{
+		// Adjust the velocity
+		switch (e.key.keysym.sym)
+		{
+		case SDLK_UP: m_VelY -= DOT_VEL; break;
+		case SDLK_DOWN: m_VelY += DOT_VEL; break;
+		case SDLK_LEFT: m_VelX -= DOT_VEL; break;
+		case SDLK_RIGHT: m_VelX += DOT_VEL; break;
+		}
+	}
+	else if (e.type == SDL_KEYUP && e.key.repeat == 0)
+	{
+		// Adjust the velocity
+		switch (e.key.keysym.sym)
+		{
+		case SDLK_UP: m_VelY += DOT_VEL; break;
+		case SDLK_DOWN: m_VelY -= DOT_VEL; break;
+		case SDLK_LEFT: m_VelX += DOT_VEL; break;
+		case SDLK_RIGHT: m_VelX -= DOT_VEL; break;
+		}
+	}
+}
+
+void Dot::move(std::vector<SDL_Rect>& colliders)
+{
+	// Move the dot left or right
+	m_PosX += m_VelX;
+	shiftColliders();
+
+	// IF the dot went too far to the left or the right or the dot collided
+	if ((m_PosX < 0) || (m_PosX + DOT_WIDTH > SCREEN_WIDTH) || CheckCollision(m_Colliders, colliders))
+	{
+		// Move back
+		m_PosX -= m_VelX;
+		shiftColliders();
+	}
+
+	// Move the dot up or down
+	m_PosY += m_VelY;
+	shiftColliders();
+
+	// IF the dot went too far to the left or the right or the dot collided
+	if ((m_PosY < 0) || (m_PosY + DOT_WIDTH > SCREEN_HEIGHT) || CheckCollision(m_Colliders, colliders))
+	{
+		// Move back
+		m_PosY -= m_VelY;
+		shiftColliders();
+	}
+}
+
+void Dot::render()
+{
+	// Show the dot
+	gDotTexture.render(m_PosX, m_PosY);
+}
+
+std::vector<SDL_Rect>& Dot::getColliders()
+{
+	return m_Colliders;
+}
+
+void Dot::shiftColliders()
+{
+	// The row offset
+	int r = 0;
+
+	// Go through the dot's collision boxes
+	for (int set = 0; set < m_Colliders.size(); set++)
+	{
+		// Center the collision box
+		m_Colliders[set].x = m_PosX + (DOT_WIDTH - m_Colliders[set].w) / 2;
+
+		// Set the collision box at its row offset
+		m_Colliders[set].y = m_PosY + r;
+
+		// Move the row offset down the height of collision box
+		r += m_Colliders[set].h;
+	}
+}
